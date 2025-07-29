280ae0a11c7b88f8e68aae458a0f6f9289b27be7
Ashwani Yadav <yadav8800766165@gmail.com>
2025-07-30 03:04:06 +0530

    first commit

--- server/seed/insertUsers.js
+++ server/seed/insertUsers.js
@@ -1,45 +1,46 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+// import 'dotenv/config'
 
-import User from "../models/user.js";
-import connectDb from "../utils/db.js";
+// import User from "../models/user.js";
+// import connectDb from "../utils/db.js";
+// console.log("MONGO_URL:", process.env.MONGO_URL);
+// const seedUsers= [
+//     {
+//         name:"Ashwani yadv",
+//     },
+//     {
+//         name:"Rahul yadv",
+//     },
+//     {
+//         name:"Shyam yadv",
+//     },
+//     {
+//         name:"Vaibhavi yadav",
+//     },{
+//         name:"Shreyansh yadav"
+//     },{
+//         name:"Priyansh yadav"
+//     },
+//     {
+//         name:"Trilok pandit",
+//     },{
+//         name:"Manish shah"
+//     },
+//     {
+//         name:"Mausam verma",
+//     },{
+//         name:"Aaryan yadav"
+//     }
+// ];
 
-const seedUsers= [
-    {
-        name:"Ashwani yadv",
-    },
-    {
-        name:"Rahul yadv",
-    },
-    {
-        name:"Shyam yadv",
-    },
-    {
-        name:"Vaibhavi yadav",
-    },{
-        name:"Shreyansh yadav"
-    },{
-        name:"Priyansh yadav"
-    },
-    {
-        name:"Trilok pandit",
-    },{
-        name:"Manish shah"
-    },
-    {
-        name:"Mausam verma",
-    },{
-        name:"Aaryan yadav"
-    }
-];
-
-const saveInDb=async()=>{
-    try {
-        await connectDb();
-        await User.insertMany(seedUsers);
-        console.log("Database seeded successfully");
-    } catch (error) {
-        console.error("Error seeding database:", error);
-    }
-}
+// const saveInDb=async()=>{
+//     try {
+//         await connectDb();
+//         await User.insertMany(seedUsers);
+//         console.log("Database seeded successfully");
+//     } catch (error) {
+//         console.error("Error seeding database:", error);
+//     }
+// }
     
-saveInDb();
+// saveInDb();
--- server/utils/db.js
+++ server/utils/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDb=async()=>{
     try {
-        mongoose.connect('mongodb://127.0.0.1:27017/leaderboard');
+        mongoose.connect(process.env.MONGO_URL);
         console.log('Connected to MongoDB');
     } catch (error) {
         console.log('Error connecting to MongoDB:', error);
